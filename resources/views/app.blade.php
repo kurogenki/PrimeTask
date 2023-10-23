02cc8e58fb99604b38ab5dd5d83072047886ff8b
kuroGenki <kurochange0522@gmail.com>
2023-10-23 14:37:20 +0900

    Https通信にした際のCSS適用の部分の削除

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,12 +10,12 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        {{-- URLをhttpsに変更して、CSSが適用されないことを防ぐための記述 --}}
+        {{-- URLをhttpsに変更して、CSSが適用されないことを防ぐための記述
         @if(config('app.env') === 'production')
             <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
         @else
             <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        @endif
+        @endif --}}
 
         <!-- Scripts -->
         @routes
